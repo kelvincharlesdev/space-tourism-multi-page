6e9f410db3b246a6a161349e7a62388524412ea0
Kelvin Charles <kelvinc1291@hotmail.com>
2024-06-17 19:32:38 -0300

    feat: add project assets

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
+import { Header } from '../../components/Header';
 import * as S from './styles';
 
 export const Home = () => {
   return (
     <S.Content>
       <S.Title>Testando</S.Title>
+      <Header>
+        <img src="src/assets/shared/logo.svg" alt="" />
+      </Header>
     </S.Content>
   );
 };
