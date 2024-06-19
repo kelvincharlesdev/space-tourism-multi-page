78642da93c5792e04d22e9cdfdc36a903becf9a6
Kelvin Charles <kelvinc1291@hotmail.com>
2024-06-18 22:04:15 -0300

    feat: adds the modal, code and style to the header for responsive version

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,3 +1,68 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.div``;
+export const HeaderContainer = styled.div`
+  height: 88px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+`;
+
+export const HeaderLogo = styled.figure`
+  width: 40px;
+  height: 40px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const ModalContent = styled.div`
+  padding: 18px;
+
+  svg {
+    cursor: pointer;
+  }
+`;
+
+export const ModalContentLogo = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+`;
+
+export const ModalNavigate = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  margin-top: 81px;
+`;
+export const Link = styled.a`
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 400;
+  font-family: ${({ theme }) => theme.fonts.family.tertiary};
+  letter-spacing: 2px;
+
+  span {
+    font-weight: 700;
+    padding-right: 12px;
+  }
+
+  &:hover {
+    border-right: 3px solid rgba(255, 255, 255, 0.5);
+  }
+
+  &:focus {
+    border-right: 3px solid #ffffff;
+  }
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,74 @@
 import * as S from './Header.styles';
-import * as I from './Header.interface';
+// import * as I from './Header.interface';
 
-export const Header = ({ children }: I.HeaderProps) => {
-  return <S.HeaderContainer>{children}</S.HeaderContainer>;
+import Modal from 'react-modal';
+
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { RiCloseLine } from 'react-icons/ri';
+import { useState } from 'react';
+import { menu } from '../../data/menu';
+
+Modal.setAppElement('#root');
+
+const customStyles = {
+  content: {
+    top: 0,
+    left: 'auto',
+    right: 0,
+    bottom: 0,
+    height: '100%',
+    minWidth: '254px',
+    marginRight: 0,
+    transform: 'none',
+    border: 'none',
+    overflowY: 'auto' as const,
+    backgroundColor: '#0B0D1726',
+    backdropFilter: 'blur(50px)',
+    padding: 0
+  },
+  overlay: {
+    backgroundColor: 'transparent'
+  }
+};
+
+export const Header = () => {
+  const [modalIsOpem, setModalIsOpen] = useState<boolean>(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <S.HeaderContainer>
+      <div className="container">
+        <S.HeaderContent>
+          <Modal isOpen={modalIsOpem} style={customStyles}>
+            <S.ModalContent>
+              <S.ModalContentLogo>
+                <RiCloseLine size={50} color="#D0D6F9" onClick={closeModal} />
+              </S.ModalContentLogo>
+
+              <S.ModalNavigate>
+                {menu.map(item => (
+                  <S.Link key={item.id} href={item.link}>
+                    <span>{item.number}</span>
+                    {item.label}
+                  </S.Link>
+                ))}
+              </S.ModalNavigate>
+            </S.ModalContent>
+          </Modal>
+
+          <S.HeaderLogo>
+            <img src="src/assets/shared/logo.svg" alt="Imagem da Logo" />
+          </S.HeaderLogo>
+          <GiHamburgerMenu size={30} color="#D0D6F9" onClick={openModal} />
+        </S.HeaderContent>
+      </div>
+    </S.HeaderContainer>
+  );
 };
