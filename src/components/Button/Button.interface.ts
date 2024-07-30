72feece63a3ad4b6591b2735de49016453fc46ec
Kelvin Charles <kelvinc1291@hotmail.com>
2024-07-29 22:09:58 -0300

    fix: Adjust button with props isActive

--- src/components/Button/Button.interface.ts
+++ src/components/Button/Button.interface.ts
@@ -3,4 +3,5 @@ import { ButtonHTMLAttributes } from 'react';
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variation?: 'primary' | 'secondary' | 'tertiary';
   text?: string;
+  isActive: boolean;
 }
